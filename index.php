2b33d05bcec8d6b20fa9848ae531ccfe25e0481f
Matt Fitch <mrf088@gmail.com>
2014-07-08 08:55:28 -0400

    initial commit. Created code to pull word list from internet

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8"> 
+
+    <title>Fitch Project 2</title>
+    <?php require 'logic.php' ?>
+
+    <link rel='stylesheet' href='styles.css' type='text/css'>
+    
+        
+</head>
+
+<body>
+
+<h1><?php randomWords(3) ?></h1>
+
+</body>
+</html>
+
+
+								
---
+++ logic.php
@@ -0,0 +1,46 @@
+<?php
+
+//initiate file array
+$lines = array();
+
+//initiate second array
+$words = array();
+
+$num1 = 1;
+$num2 = 2;
+
+for($i = 0; $i < 4; $i++)
+{
+	$tempArray = file("http://www.paulnoll.com/Books/Clear-English/words-0".($num1)."-0".($num2)."-hundred.html", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	$lines = array_merge($lines, $tempArray);
+	$num1 +=2;
+	$num2 +=2;
+}
+
+
+//second array, adding only items that come after <li>
+foreach ($lines as $line_num => $line)
+{
+	if (htmlspecialchars($line) == "								&lt;li&gt;")
+	{
+		$words[] = htmlspecialchars($lines[$line_num+1]);
+	}
+}
+
+//prints full array of words
+/*
+foreach ($words as $word_num => $word)
+{
+	echo trim($word)."<br>";
+}
+*/
+
+function randomWords($numWords)
+{
+	global $words;
+	for($i = 0; $i < $numWords; $i++)
+	{
+		echo $words[rand(0, (count($words) - 1))];
+	}
+}
+?>
